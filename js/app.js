f3f024a29c1d99ec3e8bcbdb649a2466ea0e9727
Michael Becco <e4e6d4d5@gmail.com>
2015-04-09 16:11:59 -0700

    Add .json file- Modify Title view for Input Box
    Add gitIssues to test call back

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ jQuery(function ($) {
 			this.todos = util.store('todos-jquery');
 			this.cacheElements();
 			this.bindEvents();
+			this.gitIssues();
 
 			Router({
 				'/:filter': function (filter) {
@@ -204,6 +205,16 @@ jQuery(function ($) {
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
 			this.render();
+		},
+
+		gitIssues: function() {
+		  $.getJSON( "http://ip.jsontest.com/", function( data ) {
+		    var items = [];
+		    $.each( data, function( key, val ) {
+			    $("<li id='" + key + "'>" + val + "</li>" ).appendTo($("#todo-list"));
+			    this.render();
+		    });
+    	});
 		}
 	};
 
